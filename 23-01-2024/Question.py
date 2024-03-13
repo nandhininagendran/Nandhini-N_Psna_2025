6e2351db46101f457796471132f19fd7fa203dfd
nandhininagendran <158303498+nandhininagendran@users.noreply.github.com>
2024-03-13 17:11:57 +0530

    Create Question.py

---
+++ 23-01-2024/Question.py
@@ -0,0 +1,71 @@
+At the annual meeting of Board of Directors of Acme Inc. If everyone attending shakes hands exactly one time with every other attendee, how many handshakes are there?
+
+Example
+
+There are  attendees, ,  and .  shakes hands with  and , and  shakes hands with . Now they have all shaken hands after  handshakes.
+
+Function Description
+
+Complete the handshakes function in the editor below.
+
+handshakes has the following parameter:
+
+int n: the number of attendees
+Returns
+
+int: the number of handshakes
+Input Format
+The first line contains the number of test cases .
+Each of the following  lines contains an integer, .
+
+Constraints
+
+
+
+Sample Input
+
+2
+1
+2
+Sample Output
+
+0
+1
+Explanation
+
+Case 1 : The lonely board member shakes no hands, hence 0.
+Case 2 : There are 2 board members, so 1 handshake takes place.
+"""
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'handshake' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def handshake(n):
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = handshake(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+
